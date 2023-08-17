8f85f2c424a216aa8382d1f49b966c7f9ec7a884
kyra gordon <kyra@workmail.com>
2023-08-17 12:35:18 +0100

    Initial commit (named placeholder scripts)

---
+++ KFPS_Actor.gd
@@ -0,0 +1,9 @@
+extends CharacterBody3D
+
+class_name KFPS_Actor
+
+func _ready():
+	pass
+
+func _physics_process(delta):
+	pass
---
+++ KFPS_CharacterInventory.gd
@@ -0,0 +1,9 @@
+extends Node
+
+class_name KFPS_CharacterInventory
+
+func _ready():
+	pass
+
+func _process(delta):
+	pass
