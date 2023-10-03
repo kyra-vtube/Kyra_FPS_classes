4aa4f9974bff0ebfe0db5e1030d5cbeb9fee20f8
kyra gordon <kyra@workmail.com>
2023-10-04 00:04:33 +0100

    added layer storage classes

---
+++ KFPS_CollisionLayerClass.gd
@@ -0,0 +1,9 @@
+class_name KFPS_CollisonLayerClass
+
+static var layers:Dictionary = {
+	"terrain":		0b00001,
+	"actor":		0b00010,
+	"visibility":	0b00100,
+	"damage":		0b01000,
+	"interactive":	0b10000
+}
---
+++ KFPS_RenderLayerClass.gd
@@ -0,0 +1,7 @@
+class_name KFPS_RenderLayerClass
+
+static var layers:Dictionary = {
+	"general":		0b00001,
+	"terrain":		0b00010,
+	"actors":		0b00100
+}
